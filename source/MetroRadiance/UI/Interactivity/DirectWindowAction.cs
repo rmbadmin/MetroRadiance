554a26feb5abdb7c9d402e95288f05505315e546
rmbgame <rmb@rmbgame.net>
2020-10-22 02:10:54 +0800

    update

--- source/MetroRadiance/UI/Interactivity/DirectWindowAction.cs
+++ source/MetroRadiance/UI/Interactivity/DirectWindowAction.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Interactivity;
+using Microsoft.Xaml.Behaviors;
 using MetroRadiance.UI.Controls;
 using MetroRadiance.Utilities;
 
